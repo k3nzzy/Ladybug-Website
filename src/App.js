c340acfa9b1c3bf559411e313b4ca48610ca6e24
Khavab <2021snomani@tjhsst.edu>
2022-12-02 18:58:29 -0500

    updated components folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Catch from "./pages/Catch";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/*" element={<Catch/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/*" element={<Catch/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Bug } from '@icon-park/react';
+import './Styles.css';
 
 
 const Navbar = () => {
