87f27a88350291ef756ff1cc4e5afc3d405d524e
oliver <oliverdeleong@gmail.com>
2022-12-02 00:58:32 -0500

    create Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { Bug } from '@icon-park/react';
+
+
+const Navbar = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  }
+
+  return (
+    <div className='navbar'>
+      <div className='brand-container'><Bug className='bug-logo' size={"2em"} fill={"#d51c1c"} /><a className='brand-text' href='/'>Ladybug Development Club</a></div>
+      <div onClick={toggleCollapse} className='collapseButton'>
+        <span />
+        <span />
+        <span />
+      </div>
+
+      <div className={`menu${isCollapsed ? " visible" : ""}` }>
+        <a href='/'>Home</a>
+        <a href='/'>About</a>
+        <a href='/'>Projects</a>
+        <a href='/'>FAQ</a>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Navbar;
