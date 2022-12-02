d8b3f59f91df8658c655e5e7702be1d694238c6d
Khavab <2021snomani@tjhsst.edu>
2022-12-02 15:26:02 -0500

    catch page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Catch from "./pages/Catch";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/*" element={<Catch />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Catch.js
@@ -0,0 +1,10 @@
+function Catch(){
+    
+    return (
+        <div className="App">
+          <h1>Page not found</h1>
+        </div>
+      );
+}
+
+export default Catch;
