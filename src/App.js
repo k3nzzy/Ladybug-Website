6ac16b7ac060ffb804a0077e5ac13a4aa4bae09c
oliver <oliverdeleong@gmail.com>
2022-12-02 00:57:46 -0500

    add Navbar component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Navbar />
     </div>
+    
   );
 }
 
