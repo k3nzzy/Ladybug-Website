50c360bf1ce9443226c5619a909cf25239d40a26
Khavab <shiblidnomani@gmail.com>
2023-01-02 02:37:41 -0500

    update contact page with links

--- src/pages/Contacts/Contacts.js
+++ src/pages/Contacts/Contacts.js
@@ -9,18 +9,22 @@ export default function Catch() {
             <MainPage>
                     <Title>Contact Us</Title>
                     <SubTitle>We will get back to you as soon as possible</SubTitle>
+
                     <LinkContainter>
                         <LinkText>LinkedIn</LinkText>
                         <a href="/"><img src={Vector}/></a>
                     </LinkContainter>
+
                     <LinkContainter>
                         <LinkText>Instagram</LinkText>
                         <a href="/"><img src={Vector}/></a>
                     </LinkContainter>
+
                     <LinkContainter>
                         <LinkText>OrgCentral</LinkText>
                         <a href="/"><img src={Vector}/></a>
                     </LinkContainter>
+
                     <LinkContainerB>
                         <LinkText>Email</LinkText>
                         <a href="/"><img src={Vector}/></a>
