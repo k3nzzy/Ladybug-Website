f17074d6060db4c1639782c17708507ae1b5eb2a
Khavab <shiblidnomani@gmail.com>
2023-01-07 20:49:22 -0500

    added about images

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,5 +1,6 @@
 import MainLayout from "../../components/Main Layout/Main Layout";
-import { AboutPage, Title, MajorText, MinorText, LineContainer} from "./Styles";
+import { AboutPage, Title, MajorText, MinorText, LineContainer, ProjectPage, 
+    ProjectLeft, ProjectRight, SectionTitle, SectionSubTitle, SectionContent} from "./Styles";
 import Line from "../../images/Line.svg"
 
 export default function About() {
@@ -7,12 +8,27 @@ export default function About() {
         <MainLayout>
             <AboutPage>
                 <Title>About Us</Title>
-                <MajorText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</MajorText>
+                <MajorText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem 
+                    ipsum dolor sit amet, consectetur adipiscing elit, sed 
+                    do eiusmod tempor incididunt ut labore et dolore magna aliqua</MajorText>
                 <LineContainer>
                     <img src={Line} alt="scroll line" />
                     <MinorText>Scroll for more</MinorText>
                 </LineContainer>
             </AboutPage>
+            <ProjectPage>
+                <ProjectLeft>
+                    <SectionTitle>We make something FUN</SectionTitle>
+                    <SectionSubTitle>Bring your fresh ideas to the table</SectionSubTitle>
+                    <SectionContent>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
+                        Velit officia consequat duis enim velit mollit. 
+                        Exercitation veniam consequat sunt nostrud amet.</SectionContent>
+                </ProjectLeft>
+                <ProjectRight>
+
+                </ProjectRight>
+            </ProjectPage>
         </MainLayout>
     );
 };
--- src/pages/About/Styles.js
+++ src/pages/About/Styles.js
@@ -55,12 +55,65 @@ export const LineContainer = styled.div`
 `;
 
 export const ProjectPage = styled.div`
-    height: 100vh;
+    height: 90vh;
     width: 100vw;
+    background-color: #F5F5F5;
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    align-items: flex-end;
+    flex-wrap: nowrap;
+`;
+
+export const ProjectLeft = styled.div`
+    height: 90vh;
+    width: 60vw;
+    background-color: #F5F5F5;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: baseline;
+    flex-wrap: nowrap;
+`;
+
+export const ProjectRight = styled.div`
+    height: 90vh;
+    width: 40vw;
     background-color: #F5F5F5;
     display: flex;
     flex-direction: column;
     justify-content: start;
-    align-items: flex-end;
+    align-items: flex-start;
     flex-wrap: wrap;
+`;
+
+export const SectionTitle = styled.p`
+    font-family: 'Limelight';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 40px;
+    line-height: 49px;
+    margin-left: 10%;
+`;
+
+export const SectionSubTitle = styled.p`
+    font-family: 'Rubik';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 18px;
+    line-height: 21px;
+    margin-left: 10%;
+    color: #161313
+`;
+
+export const SectionContent = styled.p`
+    font-family: 'Rubik';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 21px;
+    width: 454px;
+    margin-top: 2%;
+    margin-bottom: 2%;
+    color: #161313
 `;
